6677bc905ba64b87b62b1e537c86f408f52f4c26
KolominD <bogkolomind@gmail.com>
2021-12-04 18:29:23 +0300

    tests and Ci ADDEDÑ

--- src/test/java/ru/netology/AuthTest.java
+++ src/test/java/ru/netology/AuthTest.java
@@ -1,41 +1,81 @@
 package ru.netology;
 
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeAll;
-import io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static io.restassured.RestAssured.given;
+import static ru.netology.data.RegistrationLocalHost.getRegisteredUser;
+import static ru.netology.data.RegistrationLocalHost.getUser;
+import static ru.netology.data.getRandomLogin;
+import static ru.netology.data.getRandomPassword;
 
 public class AuthTest {
-    private static RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
-
-    @BeforeAll
-    static void setUpAll() {
-        // сам запрос
-        given() // "дано"
-                .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
-                .when() // "когда"
-                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-                .then() // "тогда ожидаем"
-                .statusCode(200); // код 200 OK
+    @BeforeEach
+    public void shouldOpen() {
+        open("http://localhost:9999/");
     }
+
     @Test
-    public void shouldTest(){
-            setUpAll();
+    public void shouldHappyPath() {
+        var registeredUser = getRegisteredUser("active");
 
+        $("[data-test-id='login']  input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password']  input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("h2").shouldBe(visible).shouldHave(exactText("Личный кабинет"));
     }
 
+    @Test
+    public void notActive() {
+        var blockedUser = getRegisteredUser("blocked");
+
+        $("[data-test-id='login']  input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password']  input").setValue(blockedUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content").shouldBe(visible).shouldHave(text("Ошибка!"));
+        $("[data-test-id='error-notification'] .notification__content").shouldBe(visible).shouldHave(text(
+                "Пользователь заблокирован"));
+    }
+
+    @Test
+    public void activeButNotReg() {
+        var notRegisteredUser = getUser("active");
+
+        $("[data-test-id='login']  input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password']  input").setValue(notRegisteredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content").shouldBe(visible).shouldHave(text("Ошибка!"));
+        $("[data-test-id='error-notification'] .notification__content").shouldBe(visible).shouldHave(text("Неверно " +
+                "указан логин или пароль"));
+    }
+
+    @Test
+    public void activeWithWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+
+        $("[data-test-id='login']  input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password']  input").setValue(wrongLogin);
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content").shouldBe(visible).shouldHave(text("Ошибка!"));
+        $("[data-test-id='error-notification'] .notification__content").shouldBe(visible).shouldHave(text("Неверно " +
+                "указан логин или пароль"));
+    }
+
+
+    @Test
+    public void activeWithWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+
+        $("[data-test-id='login']  input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password']  input").setValue(wrongPassword);
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content").shouldBe(visible).shouldHave(text("Ошибка!"));
+        $("[data-test-id='error-notification'] .notification__content").shouldBe(visible).shouldHave(text("Неверно " +
+                "указан логин или пароль"));
+    }
 }
